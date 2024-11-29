180898e75a48be15179e3c4d708a170d4b4709e5
keiz44l <keiz44l@protonmail.com>
2024-11-29 08:19:43 +0100

    init app

---
+++ app/src/main/java/com/project/triply/ui/reflow/ReflowViewModel.kt
@@ -0,0 +1,13 @@
+package com.project.triply.ui.reflow
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class ReflowViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is reflow Fragment"
+    }
+    val text: LiveData<String> = _text
+}
